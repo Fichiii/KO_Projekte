74a18df60f1f02d1eed4688f128421fe0ad67907
Fichiii <kevinehrlich31@gmail.com>
2025-11-16 23:43:44 +0100

    Projekt 3 hinzugefügt

---
+++ KO_Projekt3/SimulatedAnnealing.cpp
@@ -0,0 +1,118 @@
+#include "SimulatedAnnealing.hpp"
+
+//Setze Startlösung auf 0
+void SimulatedAnnealing::generate_firt_solution(Solution &solution, Instance& toSolve){
+    for(int i = 0; i < toSolve.n(); i++){
+        solution.set(i,0);
+    }
+}
+//Generiere random Solution in dem 1 Bit gefliped wird
+Solution SimulatedAnnealing::generate_random_solution(Solution &solution, Instance& toSolve){
+    Solution new_solution = solution;
+
+    int random_num = random_int(toSolve);
+
+    if(solution.get(random_num) == 0){
+        new_solution.set(random_num, 1);
+    }
+    else{
+        new_solution.set(random_num, 0);
+    }
+    return new_solution;
+}
+
+//Generiere Zahl zwischen 0 und n-1
+int random_int(Instance &toSolve){
+
+    static mt19937 engine(random_device{}());
+
+    uniform_int_distribution<> dist(0, toSolve.n() -1);
+
+    return dist(engine);
+}
+
+//Generiere 0 oder 1
+double random01(){
+
+    static mt19937 engine(random_device{}());
+
+    uniform_real_distribution<double> dist(0, 1);
+
+    return dist(engine);
+}
+
+void SimulatedAnnealing::print_start_solution(Solution &solution){
+    cout << "Startloesung mit Zielfunktionswert: " << solution.getValue() << '\n';
+    for(int i = 0; i < solution.getInstance().getNumberOfItems(); i++){
+        cout << solution.get(i) << ' ';
+    }
+    cout << '\n';
+}
+
+void SimulatedAnnealing::print_best_solution(Solution &solution){
+    cout << "beste gefundene Loesung mit Zielfunktionswert: " << solution.getValue() << '\n';
+    for(int i = 0; i < solution.getInstance().getNumberOfItems(); i++){
+        cout << solution.get(i) << ' ';
+    }
+    cout << '\n';
+}
+
+void SimulatedAnnealing::solve(Instance& toSolve, int timelimit, int iterationlimit, double starttemperature, double factor) {
+    Solution solution = Solution(toSolve);
+
+    generate_firt_solution(solution, toSolve);
+
+    print_start_solution(solution);
+
+    Solution best_solution = solution;
+
+    int counter_to_cool = 10 * toSolve.n();
+
+    double temperatur = starttemperature;
+
+    int best_value = best_solution.getValue();
+
+    bool stop_criteria = false;
+
+    int counter = 0;
+
+    int cooling_counter = 0;
+
+    const chrono::seconds time_limit(timelimit);
+
+    auto start_time = chrono::steady_clock::now();
+
+    auto deadline = start_time + time_limit;
+
+    while(!stop_criteria){
+        Solution new_solution = generate_random_solution(solution, toSolve);
+
+        double fraction = (new_solution.getValue() - solution.getValue())/temperatur;
+
+        if (new_solution.isFeasible()) {
+            if(new_solution.getValue() >= solution.getValue() || random01() < exp(fraction)){
+                solution = new_solution;
+
+                if(new_solution.getValue() >= best_value){
+                    best_solution = new_solution;
+                    best_value = new_solution.getValue();
+                }
+            }
+        }
+
+            counter++;
+            cooling_counter++;
+
+            if(cooling_counter == counter_to_cool){
+                temperatur *= factor;
+                cooling_counter = 0;
+            }
+
+            if(counter == iterationlimit || timelimit > 0 && chrono::steady_clock::now() >= deadline){
+                stop_criteria = true;
+            }
+        }
+
+
+    print_best_solution(best_solution);
+}
---
+++ KO_Projekt3/SimulatedAnnealing.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Instance.hpp"
+#include "Solution.hpp"
+
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include <random>
+#include <algorithm>
+#include <chrono>
+
+using namespace std;
+
+// TODO: comment this class with Doxygen (and delete this comment)
+class SimulatedAnnealing {
+
+    public:
+        static void solve(Instance& toSolve, int timelimit, int iterationlimit, double starttemperature, double factor);
+        static void generate_firt_solution(Solution &solution, Instance& toSolve);
+        static Solution generate_random_solution(Solution &solution, Instance& toSolve);
+        static void print_start_solution(Solution &solution);
+        static void print_best_solution(Solution &solution);
+};
+
+int random_int(Instance &toSolve);
+double random01();
